1666459b4f649404c05b310cf8f1b9ed5fea61d6
seán patrick john paul george ringo doran <me@spjpgrd.com>
2025-09-01 19:19:52 -0600

    Testing how theming works

---
+++ .storybook/BookplateTheme.js
@@ -0,0 +1,14 @@
+import { create } from 'storybook/theming';
+
+export default create({
+  base: 'light',
+
+  // Typography
+  fontBase: '"EB Garamond", serif',
+  fontCode: '"JetBrains Mono", monospace',
+
+  brandTitle: 'Bookplate',
+  brandUrl: 'https://68b627f48974895b1004bad0-wosxwdfvfy.chromatic.com',
+  brandImage: '/stories/assets/bookplate-brand-image.png',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from 'storybook/manager-api';
+import BookplateTheme from './BookplateTheme';
+
+addons.setConfig({
+  theme: BookplateTheme,
+});
